63a7a368cb7d88ba23fe6bcd854ec58873639453
Rohit <rohitjha941@hotmail.com>
2019-03-31 21:45:49 +0530

    Updates

---
+++ precache-manifest.0cb445c7ff346b3d0afc7e1f5a2bdb12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "717391737e6c50302c8d09a772041f08",
+    "url": "/static/media/rethink.71739173.png"
+  },
+  {
+    "revision": "3523756b6c55b55d5ec73703f1fa7384",
+    "url": "/static/media/background.3523756b.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "939e9b37c8e0f3d97320",
+    "url": "/static/js/main.79e8d503.chunk.js"
+  },
+  {
+    "revision": "5cebeea094ba5c82064d",
+    "url": "/static/js/2.0e0eb093.chunk.js"
+  },
+  {
+    "revision": "939e9b37c8e0f3d97320",
+    "url": "/static/css/main.eecd6ee5.chunk.css"
+  },
+  {
+    "revision": "6d751df037afadc4bf2edfcaf71b02db",
+    "url": "/index.html"
+  }
+];
